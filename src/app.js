6ed80d2e9a6c0c5562c3c80aa620e2b0abdb2b15
Tira Odhner <atiraodhner@gmail.com>
2017-09-18 15:04:19 -0400

    Convert to camelcase

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
-const draw_emoji = function(ctx, {eyes, mouth}, x, y, diameter){
+const drawEmoji = function(ctx, {eyes, mouth}, x, y, diameter){
 
   const radius = diameter / 2;
 
-  const yellow_gradient = ctx.createLinearGradient(0, -radius, 0, diameter);
+  const yellowGradient = ctx.createLinearGradient(0, -radius, 0, diameter);
 
-  yellow_gradient.addColorStop(0.0, '#fcf5b5');
-  yellow_gradient.addColorStop(0.5, '#f1da36');
-  yellow_gradient.addColorStop(1.0, '#f4c838');
+  yellowGradient.addColorStop(0.0, '#fcf5b5');
+  yellowGradient.addColorStop(0.5, '#f1da36');
+  yellowGradient.addColorStop(1.0, '#f4c838');
   
   ctx.save();
   ctx.translate(x, y);
 
   ctx.beginPath();
   ctx.arc(0, 0, radius, 0, TAU);
-  ctx.fillStyle = yellow_gradient;
+  ctx.fillStyle = yellowGradient;
   ctx.fill();
   ctx.beginPath();
   ctx.arc(0, 5, radius * 0.92, 0, TAU);
@@ -36,12 +36,12 @@ const draw_emoji = function(ctx, {eyes, mouth}, x, y, diameter){
     //ctx.lineTo(+radius/2*0.9, radius*0.4)
     //ctx.lineTo(+radius/2, radius*0.2)
 
-    const smile_y = mouth.smile * radius*0.2;
+    const smileY = mouth.smile * radius*0.2;
 
     for (i = 0; i <= 1; i += 0.1) {
       ctx.lineTo(
         (cos(i * PI) * radius)/2,
-        ((((sin(i * PI) * radius)/2) * mouth.smile) + (radius*0.2)) - smile_y
+        ((((sin(i * PI) * radius)/2) * mouth.smile) + (radius*0.2)) - smileY
       );
     }
 
@@ -63,7 +63,7 @@ const draw_emoji = function(ctx, {eyes, mouth}, x, y, diameter){
       for (i = 0; i <= 1; i += 0.1) {
         ctx.lineTo(
           (cos(i * PI) * radius)/3,
-          (((sin(i * PI) * radius)/2) * mouth.tongue) + (radius*0.2) + (smile_y * (mouth.open ? 0 : 1))
+          (((sin(i * PI) * radius)/2) * mouth.tongue) + (radius*0.2) + (smileY * (mouth.open ? 0 : 1))
         );
       }
 
@@ -80,10 +80,10 @@ const draw_emoji = function(ctx, {eyes, mouth}, x, y, diameter){
   // TODO: add line down middle of tongue
 
 
-  const draw_eye = function(eye, x, y){
+  const drawEye = function(eye, x, y){
     ctx.beginPath();
-    x += eye.offset_x * radius;
-    y += eye.offset_y * radius;
+    x += eye.offsetX * radius;
+    y += eye.offsetY * radius;
     switch (eye.type) {
       case "open":
         ctx.arc(x, y, radius/6, 0, TAU);
@@ -99,10 +99,10 @@ const draw_emoji = function(ctx, {eyes, mouth}, x, y, diameter){
   };
 
   if (eyes != null ? eyes.left : undefined) {
-    draw_eye(eyes.left, -radius*0.4, -radius/3);
+    drawEye(eyes.left, -radius*0.4, -radius/3);
   }
   if (eyes != null ? eyes.right : undefined) {
-    draw_eye(eyes.right, +radius*0.4, -radius/3);
+    drawEye(eyes.right, +radius*0.4, -radius/3);
   }
 
 
@@ -110,24 +110,24 @@ const draw_emoji = function(ctx, {eyes, mouth}, x, y, diameter){
 };
 
 
-const make_random_emoji = function() {
-  const choose_random = array=> array[~~(Math.random() * array.length)];
+const makeRandomEmoji = function() {
+  const chooseRandom = array=> array[~~(Math.random() * array.length)];
 
-  const random_eye = () =>
+  const randomEye = () =>
   ({
     type: Math.random() < 0.3 ? "wink" : "open",
-    offset_x: (Math.random() - 0.5),
-    offset_y: (Math.random() - 0.5)
+    offsetX: (Math.random() - 0.5),
+    offsetY: (Math.random() - 0.5)
   })
   ;
   return {
     eyes: {
-      left: random_eye(),
-      right: random_eye()
+      left: randomEye(),
+      right: randomEye()
     },
     // TODO: offsets or whatever
     // TODO: hearts, dollars, stars
-    // also: XD  >_<  ^_^  ._.  8)  B)
+    // also: XD  >_<  ^^  ._.  8)  B)
     // what about spider eyes? ::::)
     mouth: {
       smile: Math.random() - Math.random(), // -1..1 (negative for frown)
@@ -137,7 +137,7 @@ const make_random_emoji = function() {
       tongue: // -1..1? TODO: offsets or angle or whatever
         Math.random() < 0.7 ?
           // Math.random() - Math.random()
-          choose_random([-1, -0.5, 0.5, 1])
+          chooseRandom([-1, -0.5, 0.5, 1])
           : 0
     } // for dat.gui's benefit
   };
@@ -149,18 +149,18 @@ const make_random_emoji = function() {
 // TODO: animation (maybe have an update method on these objects? or have higher level descriptors?)
 
 
-const emojis_container = document.getElementById("emojis");
+const emojisContainer = document.getElementById("emojis");
 
-let selected_emoji_display = null;
+let selectedEmojiDisplay = null;
 
-const make_emoji_display = function(emoji){
+const makeEmojiDisplay = function(emoji){
 
   let update;
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
 
   canvas.classList.add("emoji");
-  emojis_container.appendChild(canvas);
+  emojisContainer.appendChild(canvas);
 
   const size = 150;
   const spacing = (size * 0.1) + 5;
@@ -169,54 +169,54 @@ const make_emoji_display = function(emoji){
 
   (update = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  return draw_emoji(ctx, emoji, canvas.width/2, canvas.height/2, size);
+  return drawEmoji(ctx, emoji, canvas.width/2, canvas.height/2, size);
 })();
 
-  const this_emoji_display = {emoji, canvas, ctx, size, update};
+  const thisEmojiDisplay = {emoji, canvas, ctx, size, update};
 
   canvas.style.cursor = "pointer";
   canvas.addEventListener("click", e=> {
-    selected_emoji_display = this_emoji_display;
-  return toggle_dat_gui_for_emoji(emoji);
+    selectedEmojiDisplay = thisEmojiDisplay;
+  return toggleDatGuiForEmoji(emoji);
 });
 
-  return this_emoji_display;
+  return thisEmojiDisplay;
 };
 
 
-let dat_gui = null;
-let dat_gui_open_for_emoji = null;
-var toggle_dat_gui_for_emoji = function(emoji){
-  if (dat_gui && (dat_gui_open_for_emoji === emoji)) {
-    dat_gui.destroy();
-    return dat_gui = null;
+let datGui = null;
+let datGuiOpenForEmoji = null;
+var toggleDatGuiForEmoji = function(emoji){
+  if (datGui && (datGuiOpenForEmoji === emoji)) {
+    datGui.destroy();
+    return datGui = null;
   } else {
-    return open_dat_gui_for_emoji(emoji);
+    return openDatGuiForEmoji(emoji);
   }
 };
-var open_dat_gui_for_emoji = function(emoji){
-  if (dat_gui != null) {
-    dat_gui.destroy();
+var openDatGuiForEmoji = function(emoji){
+  if (datGui != null) {
+    datGui.destroy();
   }
-  dat_gui = new dat.GUI();
-  dat_gui_open_for_emoji = emoji;
-  const mouth_folder = dat_gui.addFolder('Mouth');
-  mouth_folder.open();
-  mouth_folder.add(emoji.mouth, 'smile', -1, +1).name('Smile');
-  mouth_folder.add(emoji.mouth, 'open').name('Open');
-  mouth_folder.add(emoji.mouth, 'tongue', -1, +1).step(0.5);
-  const eyes_folder = dat_gui.addFolder('Eyes');
-  eyes_folder.open();
-  eyes_folder.add(emoji.eyes.left, 'type', {"Open": "open", "Wink": "wink"}).name('Left Eye');
-  eyes_folder.add(emoji.eyes.left, 'offset_x', -1, 1).name('Left Eye X');
-  eyes_folder.add(emoji.eyes.left, 'offset_y', -1, 1).name('Left Eye Y');
-  eyes_folder.add(emoji.eyes.right, 'type', {"Open": "open", "Wink": "wink"}).name('Right Eye');
-  eyes_folder.add(emoji.eyes.right, 'offset_x', -1, 1).name('Right Eye X');
-  return eyes_folder.add(emoji.eyes.right, 'offset_y', -1, 1).name('Right Eye Y');
+  datGui = new dat.GUI();
+  datGuiOpenForEmoji = emoji;
+  const mouthFolder = datGui.addFolder('Mouth');
+  mouthFolder.open();
+  mouthFolder.add(emoji.mouth, 'smile', -1, +1).name('Smile');
+  mouthFolder.add(emoji.mouth, 'open').name('Open');
+  mouthFolder.add(emoji.mouth, 'tongue', -1, +1).step(0.5);
+  const eyesFolder = datGui.addFolder('Eyes');
+  eyesFolder.open();
+  eyesFolder.add(emoji.eyes.left, 'type', {"Open": "open", "Wink": "wink"}).name('Left Eye');
+  eyesFolder.add(emoji.eyes.left, 'offsetX', -1, 1).name('Left Eye X');
+  eyesFolder.add(emoji.eyes.left, 'offsetY', -1, 1).name('Left Eye Y');
+  eyesFolder.add(emoji.eyes.right, 'type', {"Open": "open", "Wink": "wink"}).name('Right Eye');
+  eyesFolder.add(emoji.eyes.right, 'offsetX', -1, 1).name('Right Eye X');
+  return eyesFolder.add(emoji.eyes.right, 'offsetY', -1, 1).name('Right Eye Y');
 };
 
 
-const emoji_displays = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => make_emoji_display(make_random_emoji())));
+const emojiDisplays = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((i) => makeEmojiDisplay(makeRandomEmoji())));
 
-animate(() => selected_emoji_display != null ? selected_emoji_display.update() : undefined);
+animate(() => selectedEmojiDisplay != null ? selectedEmojiDisplay.update() : undefined);
 
