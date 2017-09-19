9d821b98bd23fa007e18b49963ec3261cb8fea69
Tira Odhner <atiraodhner@gmail.com>
2017-09-19 00:21:02 -0400

    Animate between expressions, though it uses setTimeout

---
+++ docs/smile-factory.js
@@ -0,0 +1,23 @@
+
+const makeSmileExpression = (smileAmount) => {
+  return {
+    eyes: {
+      left: {
+        type: "open",
+        offsetX: 0,
+        offsetY: 0
+      },
+      right: {
+        type: "open",
+        offsetX: 0,
+        offsetY: 0
+      }
+    },
+    mouth: {
+      smile: smileAmount,
+      open: false,
+      tongue: 0
+    }
+  };
+};
+export default makeSmileExpression;
